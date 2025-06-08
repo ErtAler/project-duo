98ae0dd717dc81591320856f01b15ef05afe30b6
ErtAler <slavanamaev@gmail.com>
2025-06-08 17:35:39 +0600

    Сделал модель для блюд, так же сделал миграцию

--- core/dishes/models.py
+++ core/dishes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import CASCADE
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Dishes(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='dishes_image/', null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} — {self.price}"
+
+
